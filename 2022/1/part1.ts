3f533b1221888c9bb4004d39321efbda83e02406
Arnel Bucio <rnlbucio@gmail.com>
2022-12-05 19:55:10 +0800

    Day1 Cleanup

--- 2022/1/part1.ts
+++ 2022/1/part1.ts
@@ -1,15 +1,16 @@
 const fs = require('fs')
+const path = require('path')
 
-const input = fs.readFileSync('input.txt', 'utf8')
-const inputArray = input.split('\n\n')
+const input = fs.readFileSync(path.resolve(__dirname, './input.txt'), 'utf8')
+const elvesArray: string[] = input.split('\n\n')
 
 
-const elfCalories = inputArray
+const elfCalories = elvesArray
   .map(elf => {
     return elf
       .split('\n')
       .map(calorie => parseInt(calorie))
-      .reduce((sum, currentValue) => sum+ currentValue, 0)
+      .reduce((sum, currentValue) => sum + currentValue, 0)
   })
 const highestCalorie = Math.max(...elfCalories)
 
--- 2022/1/part2.ts
+++ 2022/1/part2.ts
@@ -1,8 +1,9 @@
 export {}
 
 const fs = require('fs')
+const path = require('path')
 
-const input = fs.readFileSync('input.txt', 'utf8')
+const input = fs.readFileSync(path.resolve(__dirname, './input.txt'), 'utf8')
 const inputArray: string[] = input.split('\n\n')
 
 
